427df45e5f527a9c7ea726d9b87c7bcf47fc4ba2
Adarsh Singh <hsrada23@gmail.com>
2024-02-02 18:28:40 +0530

    Refactored Login endpoint handlers

--- login/router.py
+++ login/router.py
@@ -2,58 +2,22 @@ from datetime import datetime
 from typing import Annotated
 
 from fastapi import APIRouter, status, Request , Response, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import RedirectResponse
 from sqlmodel import Session, select
-from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
 
 from database.utils import get_passhash
 from database.engine import engine
 from models.auth import User, UserData, LoginLogs, LoginLogsCreate, ResetPasswordModel
-from .jwt_utils import generate_jwt, get_playload_from_token
+from .jwt_utils import generate_jwt
+from .utils import get_token_data, get_validated_user, get_validated_user_resetpass, get_loggedin_user
+
+from fastapi.security import OAuth2PasswordBearer
 
 login_router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-def get_current_user(userData: UserData):
-    with Session(engine) as session:
-        users = session.exec(select(User).where(User.username == userData.username))
-        user = users.first()
-
-        # User does not exist
-        if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
-        
-        #User is locked
-        if user.is_locked():
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='User is locked due to multiple invalid login attempts')
-
-        return user
-
-def validate_user_login(user: Annotated[User, Depends(get_current_user)], userData: UserData ):
-    with Session(engine) as session:
-        # Password Incorrect
-        if (user.password != get_passhash(userData.password)):
-            user.failed_attempts += 1
-            session.add(user)
-            session.commit()
-            remaining_attempts = user.remaining_attempts()
-
-            raise HTTPException(detail=f'Incorrect password, you have {remaining_attempts} attempt{"" if remaining_attempts == 1 else "s" } left.',
-                                    status_code=status.HTTP_403_FORBIDDEN)
-        return user
-
-
-def validate_user_reset_pass(user: Annotated[User, Depends(get_current_user)], userData: UserData ):
-    with Session(engine) as session:
-        # Password Incorrect
-        if (user.password != get_passhash(userData.password)):
-            return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Incorrect Password')
-        return user
-
-
 @login_router.post('/login')
-def handle_login(user: Annotated[User, Depends(validate_user_login)], request: Request ):
+def handle_login(user: Annotated[User, Depends(get_validated_user)], request: Request ):
     with Session(engine) as session:
         if user.password_change_needed():
             return RedirectResponse('reset_password', status_code=status.HTTP_303_SEE_OTHER) 
@@ -81,7 +45,7 @@ def get_reset():
     return 'RESET PAGE HERE'
 
 @login_router.post('/reset_password')
-def handle_reset(userData: ResetPasswordModel,user: Annotated[User, Depends(validate_user_reset_pass)]):
+def handle_reset(userData: ResetPasswordModel,user: Annotated[User, Depends(get_validated_user_resetpass)]):
     with Session(engine) as session:
         # Current details are valid, Changing password
         user.password = get_passhash(userData.new_password)
@@ -107,31 +71,17 @@ def handle_register(userData: UserData, response: Response):
 
 
 @login_router.get('/refresh_token')
-def refresh_token(token: Annotated[str, Depends(oauth2_scheme)], response:Response):
+def refresh_token(data: Annotated[str, Depends(get_token_data)], response:Response):
     '''
     Takes a token from the request, validates it and sends back a new token
     '''
-    try:
-        token_payload = get_playload_from_token(token)
-    except ExpiredSignatureError:
-        return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='The login token has expired.')
-    except:
-        return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='The login token is not valid')
-
-    return {'token':generate_jwt(username=token_payload['sub'])}
+    return {'token':generate_jwt(username=data['sub'])}
 
 
 # DEBUG FUNCTION - checks if the token is valid
 @login_router.get('/check_login')
-def secure_endpoint(token: Annotated[str, Depends(oauth2_scheme)], response : Response):
+def secure_endpoint(user: Annotated[User, Depends(get_loggedin_user)], response : Response):
     '''
     Checks if user is logged in.
     '''
-    try:
-        get_playload_from_token(token)
-    except ExpiredSignatureError:
-        return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='The login token has expired. You need to log in again.')
-    except InvalidSignatureError:
-        return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='The login session is invalid. You need to log in again.')
-
-    return 'Logged In'
+    return 'Logged In as ' + user.username
---
+++ login/utils.py
@@ -0,0 +1,62 @@
+from login.jwt_utils import get_playload_from_token
+from models.auth import User, UserData
+from sqlmodel import Session, select
+from fastapi import HTTPException, status, Depends
+from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
+
+from database.utils import get_passhash
+from database.engine import engine
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+def __get_current_user(userData: UserData):
+    with Session(engine) as session:
+        users = session.exec(select(User).where(User.username == userData.username))
+        user = users.first()
+
+        # User does not exist
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+        
+        #User is locked
+        if user.is_locked():
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='User is locked due to multiple invalid login attempts')
+
+        return user
+
+def get_validated_user(user: Annotated[User, Depends(__get_current_user)], userData: UserData ):
+    with Session(engine) as session:
+        # Password Incorrect
+        if (user.password != get_passhash(userData.password)):
+            user.failed_attempts += 1
+            session.add(user)
+            session.commit()
+            remaining_attempts = user.remaining_attempts()
+
+            raise HTTPException(detail=f'Incorrect password, you have {remaining_attempts} attempt{"" if remaining_attempts == 1 else "s" } left.',
+                                    status_code=status.HTTP_403_FORBIDDEN)
+        return user
+
+
+def get_validated_user_resetpass(user: Annotated[User, Depends(__get_current_user)], userData: UserData ):
+    with Session(engine) as session:
+        # Password Incorrect
+        if (user.password != get_passhash(userData.password)):
+            return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Incorrect Password')
+        return user
+
+def get_token_data(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        data = get_playload_from_token(token)
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='The login token has expired. You need to log in again.')
+    except InvalidSignatureError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='The login session is invalid. You need to log in again.')
+    return data
+
+
+def get_loggedin_user(data: Annotated[dict, Depends(get_token_data)]):
+    with Session(engine) as session:
+        return session.exec(select(User).where(User.username == data['sub'])).first()
